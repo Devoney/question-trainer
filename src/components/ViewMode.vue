f8ae35e31631bae9bcfcde29546acfc2501e0dc6
Mik de Klerk <mikedeklerk@gmail.com>
2019-09-21 18:11:17 +0200

    More DRY solution by putting a view mode drop down item in its own component: ViewModeItem.vue.

--- src/components/ViewMode.vue
+++ src/components/ViewMode.vue
@@ -5,30 +5,9 @@
         View mode
       </button>
       <div class="dropdown-menu">
-        <a :class="[{ disabled: viewModeIs('both')}, 'dropdown-item']" href="#" :disabled="store.viewMode === 'both'" @click="setViewMode('both')">
-          <div class="row">
-            <div class="col-3">
-              <font-awesome-icon icon="columns" />
-            </div>
-            <div class="col">Both</div>
-          </div>
-        </a>
-        <a :class="[{ disabled: viewModeIs('library')}, 'dropdown-item']" href="#" :disabled="store.viewMode === 'library'" @click="setViewMode('library')">
-          <div class="row">
-            <div class="col-3">
-              <font-awesome-icon icon="book" />
-            </div>
-            <div class="col">Library only</div>
-          </div>
-        </a>
-        <a :class="[{ disabled: viewModeIs('questions')}, 'dropdown-item']" href="#" @click="setViewMode('questions')">
-          <div class="row">
-            <div class="col-3">
-              <font-awesome-icon icon="question-circle" />
-            </div>
-            <div class="col">Questions only</div>
-          </div>
-        </a>
+        <view-mode-item icon="columns" text="Both" view-mode="both"/>
+        <view-mode-item icon="book" text="Library only" view-mode="library"/>
+        <view-mode-item icon="question-circle" text="Questions only" view-mode="questions"/>
       </div>
     </div>
   </div>
@@ -39,19 +18,13 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import { mixins } from 'vue-class-component';
 import StoreMixin from '@/mixins/StoreMixin';
 import MutationTypes from '@/state/MutationTypes';
-
+import ViewModeItem from '@/components/ViewModeItem.vue';
 @Component({
   components: {
+    ViewModeItem,
   },
 })
 export default class ViewMode extends mixins(StoreMixin) {
-  private viewModeIs(viewMode: string): boolean {
-    return this.store.state.viewMode === viewMode;
-  }
-
-  private setViewMode(viewMode: string): void {
-    this.store.commit(MutationTypes.viewMode, viewMode);
-  }
 }
 </script>
 
