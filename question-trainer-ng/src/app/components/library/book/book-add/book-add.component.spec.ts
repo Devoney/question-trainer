6a5b29f5e953f1eef5ee34b0871d2d58db8d5ecb
Mike de Klerk <mikedeklerk@gmail.com>
2020-04-27 12:35:17 +0200

    Adds unit tests for book-add to test that the book add event is emitted under correct circumstances.

--- question-trainer-ng/src/app/components/library/book/book-add/book-add.component.spec.ts
+++ question-trainer-ng/src/app/components/library/book/book-add/book-add.component.spec.ts
@@ -1,15 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { BookAddComponent } from './book-add.component';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 describe('BookAddComponent', () => {
   let component: BookAddComponent;
   let fixture: ComponentFixture<BookAddComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ BookAddComponent ],
+      imports: [
+        ReactiveFormsModule
+      ],
       providers:[
         FormBuilder,
       ]
@@ -21,9 +25,84 @@ describe('BookAddComponent', () => {
     fixture = TestBed.createComponent(BookAddComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    nativeElement = fixture.debugElement.nativeElement as HTMLElement;
   });
+
+  function bookTitleInputElement(): HTMLInputElement {
+    return nativeElement.querySelector('input[formcontrolname="bookTitle"]') as HTMLInputElement;
+  }
+
+  function getBookTitle(): string {
+    const bookTitleInput = bookTitleInputElement();
+    return bookTitleInput.value;
+  }
+
+  function setBookTitle(value: string): void {
+    const bookTitleInput = bookTitleInputElement();
+    bookTitleInput.value = value;
+    bookTitleInput.dispatchEvent(new Event('input'));
+    component.bookTitleIsEmpty$.next(!value); // TODO: Would be nice not to manage this here.
+    fixture.detectChanges();
+  }
+
+  function clickAddButton() {
+    const buttonId: string = '#btn-ok-book';
+    const addButton = nativeElement.querySelector(buttonId) as HTMLButtonElement;
+    if (addButton.disabled) {
+      console.log('WARNING: ' + buttonId + ' is disabled');
+    }
+    addButton.click();
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('User interaction', () => {
+    it('When user presses add button, without title, no event is raised.', () => {
+      // Given
+      setBookTitle('');
+      let addBookEvent = false;
+      component.addBook.subscribe((bookTitle: string) => {
+        addBookEvent = true;
+      });
+
+      // When
+      clickAddButton();
+
+      //Then
+      expect(addBookEvent).toBeFalse();
+    });
+
+    it('When user presses add button, with title, add event is raised with book title as argument.', () => {
+      // Given
+      const expectedBookTitle = 'My big TOE';
+      let actualBookTitle: string = null;
+      setBookTitle(expectedBookTitle);
+      let addBookEvent = false;
+      component.addBook.subscribe((bookTitle: string) => {
+        addBookEvent = true;
+        actualBookTitle = bookTitle;
+      });
+
+      // When
+      clickAddButton();
+
+      //Then
+      expect(addBookEvent).toBeTrue();
+      expect(actualBookTitle).toBe(expectedBookTitle);
+    });
+
+    it('When user adds book, the title is emptied.', () => {
+      // Given
+      setBookTitle('My big TOE');      
+
+      // When
+      clickAddButton();
+      const actualBookTitle = getBookTitle();
+
+      //Then
+      expect(actualBookTitle).toBeFalsy();
+    });
   });
 });
