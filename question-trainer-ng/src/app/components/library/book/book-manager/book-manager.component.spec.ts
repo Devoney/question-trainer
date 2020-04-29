22c4057793069af47028f8c5a39904db0e1e8716
Mike de Klerk <mikedeklerk@gmail.com>
2020-04-29 16:27:52 +0200

    Fixes the lat failing test due to a missing store dependency.

--- question-trainer-ng/src/app/components/library/book/book-manager/book-manager.component.spec.ts
+++ question-trainer-ng/src/app/components/library/book/book-manager/book-manager.component.spec.ts
@@ -1,16 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { BookManagerComponent } from './book-manager.component';
+import { IAppState } from 'src/app/store/state/app.state';
 
 describe('BookManagerComponent', () => {
   let component: BookManagerComponent;
   let fixture: ComponentFixture<BookManagerComponent>;
+  let store: MockStore;
+
+  const initialState: IAppState = {
+    library: {
+      books: []
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookManagerComponent ]
+      declarations: [ BookManagerComponent ],
+      providers: [
+        provideMockStore({ initialState })
+      ]
     })
     .compileComponents();
+
+    store = TestBed.inject(MockStore);
   }));
 
   beforeEach(() => {
