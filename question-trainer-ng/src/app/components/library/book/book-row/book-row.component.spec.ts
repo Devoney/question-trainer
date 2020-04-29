3efb29eeddf5984a3cbcfd1d24d43673bb75d5c4
Mike de Klerk <mike.deklerk@amadeus.com>
2020-04-29 15:01:43 +0200

    Fixes some unit tests that failed due to a missing store dependency.

--- question-trainer-ng/src/app/components/library/book/book-row/book-row.component.spec.ts
+++ question-trainer-ng/src/app/components/library/book/book-row/book-row.component.spec.ts
@@ -1,21 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { BookRowComponent } from './book-row.component';
+import { IAppState } from 'src/app/store/state/app.state';
+import { Guid } from 'src/tools/Guid';
+import { Book } from 'src/app/types/Book';
 
 describe('BookRowComponent', () => {
   let component: BookRowComponent;
   let fixture: ComponentFixture<BookRowComponent>;
+  let store: MockStore;
+
+  const initialState: IAppState = {
+    library: {
+      books: []
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookRowComponent ]
+      declarations: [ BookRowComponent ],
+      providers: [
+        provideMockStore({ initialState })
+      ]
     })
     .compileComponents();
+
+    store = TestBed.inject(MockStore);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookRowComponent);
     component = fixture.componentInstance;
+    const book: Book = {
+      title: 'My title',
+      id: Guid.newGuid()
+    };
+    component.book = book;
     fixture.detectChanges();
   });
 
--- question-trainer-ng/src/app/components/library/book/books-overview/books-overview.component.spec.ts
+++ question-trainer-ng/src/app/components/library/book/books-overview/books-overview.component.spec.ts
@@ -1,16 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { BooksOverviewComponent } from './books-overview.component';
+import { IAppState } from 'src/app/store/state/app.state';
 
 describe('BooksOverviewComponent', () => {
   let component: BooksOverviewComponent;
   let fixture: ComponentFixture<BooksOverviewComponent>;
+  let store: MockStore;
+
+  const initialState: IAppState = {
+    library: {
+      books: []
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BooksOverviewComponent ]
+      declarations: [ BooksOverviewComponent ],
+      providers: [
+        provideMockStore({ initialState })
+      ]
     })
     .compileComponents();
+
+    store = TestBed.inject(MockStore);
   }));
 
   beforeEach(() => {
