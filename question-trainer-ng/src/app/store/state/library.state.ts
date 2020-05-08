17f19464aa260bf6c13b0b2d0fbca7b92e5da0df
Mike de Klerk <mikedeklerk@gmail.com>
2020-05-08 12:53:50 +0200

    Adds test data, when configured.

--- question-trainer-ng/src/app/store/state/library.state.ts
+++ question-trainer-ng/src/app/store/state/library.state.ts
@@ -1,4 +1,6 @@
 import { Book } from '../../types/book';
+import { environment } from 'src/environments/environment';
+import { getBookWithTitle, getRandomBookWithChapters } from 'test/library';
 
 export interface ILibraryState {
   books: Book[];
@@ -11,3 +13,9 @@ export const initialLibraryState: ILibraryState = {
   bookIdToEdit: null,
   bookIdSelected: null,
 };
+
+if (environment.addTestData) {  
+  initialLibraryState.books.push(getRandomBookWithChapters(3));
+  initialLibraryState.books.push(getRandomBookWithChapters(5));
+  initialLibraryState.books.push(getRandomBookWithChapters(7));
+}
