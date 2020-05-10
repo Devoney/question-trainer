628fa33a22c9edb7a3ab290ac9383e40e66b0d29
Mike de Klerk <mikedeklerk@gmail.com>
2020-05-10 16:40:11 +0200

    Tests that a chapter-row is rendered for each chapter in the selected book.

--- question-trainer-ng/src/app/components/library/chapter/chapter-overview/chapter-overview.component.spec.ts
+++ question-trainer-ng/src/app/components/library/chapter/chapter-overview/chapter-overview.component.spec.ts
@@ -2,14 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChapterOverviewComponent } from './chapter-overview.component';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { getRandomBook } from 'test/library';
+import { getRandomBook, getRandomBookWithChapters } from 'test/library';
 import { getStateWithBooks } from 'test/store';
+import { selectChaptersOfSelectedBook } from 'src/app/store/selectors/library.selectors';
 
 describe('ChapterOverviewComponent', () => {
   let component: ChapterOverviewComponent;
   let fixture: ComponentFixture<ChapterOverviewComponent>;
   let store: MockStore;
-  const book = getRandomBook();
+  let nativeElement: HTMLElement;
+  const book = getRandomBookWithChapters(3);
   const initialState = getStateWithBooks(book);
 
   beforeEach(async(() => {
@@ -24,15 +26,26 @@ describe('ChapterOverviewComponent', () => {
     .compileComponents();
 
     store = TestBed.inject(MockStore);
+    store.overrideSelector(selectChaptersOfSelectedBook, book.chapters);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChapterOverviewComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should render a row for each chapter of the selected book.', () => {
+    // Given
+    // When
+    const nrOfChapterRows = nativeElement.querySelectorAll('tr[app-chapter-row]').length;
+
+    // Then
+    expect(nrOfChapterRows).toBe(book.chapters.length);
   });
 });
