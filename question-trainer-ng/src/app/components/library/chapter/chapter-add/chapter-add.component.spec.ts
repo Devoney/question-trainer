a67f7bf05e0a98cbdeea1c1214a30f106c26fc6d
Mike de Klerk <mikedeklerk@gmail.com>
2020-05-10 13:51:48 +0200

    Adds unit test coverage for the chapter-add component with respect to conditions when to add or show error messages.

--- question-trainer-ng/src/app/components/library/chapter/chapter-add/chapter-add.component.spec.ts
+++ question-trainer-ng/src/app/components/library/chapter/chapter-add/chapter-add.component.spec.ts
@@ -8,7 +8,9 @@ import { getRandomBookWithChapters } from 'test/library';
 import { BehaviorSubject } from 'rxjs';
 import { IAppState } from 'src/app/store/state/app.state';
 import { AddChapter, ChaptersActionTypes } from 'src/app/store/actions/chapters.actions';
-import { setTextInput } from 'test/form-controls';
+import { setTextInput, getTitleOfElement } from 'test/form-controls';
+import { provideI18nServiceMock } from 'test/I18nService';
+import { i18n } from 'src/app/enums/i18n';
 
 describe('ChapterAddComponent', () => {
   let component: ChapterAddComponent;
@@ -31,6 +33,7 @@ describe('ChapterAddComponent', () => {
       providers: [
         FormBuilder,
         provideMockStore({ initialState }),
+        provideI18nServiceMock(),
       ]
     })
     .compileComponents();
@@ -49,11 +52,11 @@ describe('ChapterAddComponent', () => {
     };
     store.setState(state);
   }
-  
+
   function setNr(nr: string): void {
     setTextInput(nativeElement, '[formcontrolname="nr"]', nr);
   }
-  
+
   function setTitle(title: string): void {
     setTextInput(nativeElement, '[formcontrolname="title"]', title);
   }
@@ -74,29 +77,190 @@ describe('ChapterAddComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Chapter should be added.', () => {
-    // Given
-    setSelectedBook(book2.id);
-    const chapterNr = book2.chapters.length + 1 + '';
-    setNr(chapterNr);
-    const title = 'My title';
-    setTitle(title);
-    let action: AddChapter;
-    store.scannedActions$.subscribe(a => {
-      if(a.type === ChaptersActionTypes.Add) {
-        action = a as AddChapter;
-      }
+  describe('Chapter addition', () => {
+    it('Chapter should be added, with correct data.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const chapterNr = book2.chapters.length + 1 + '';
+      setNr(chapterNr);
+      const title = 'My title';
+      setTitle(title);
+      let action: AddChapter;
+      store.scannedActions$.subscribe(a => {
+        if (a.type === ChaptersActionTypes.Add) {
+          action = a as AddChapter;
+        }
+      });
+      fixture.detectChanges();
+
+      // When
+      clickOkButton();
+
+      // Then
+      expect(action).toBeDefined();
+      expect(action.bookId).toBe(book2.id);
+      expect(action.chapter.nr).toBe(chapterNr);
+      expect(action.chapter.title).toBe(title);
+      expect(action.chapter.id).toBeDefined();
     });
-    fixture.detectChanges();
-  
-    // When
-    clickOkButton();
-  
-    // Then
-    expect(action).toBeDefined();
-    expect(action.bookId).toBe(book2.id);
-    expect(action.chapter.nr).toBe(chapterNr);
-    expect(action.chapter.title).toBe(title);
-    expect(action.chapter.id).toBeDefined();
+
+    it('Chapter should not be added when nr is empty.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const title = 'My title';
+      setTitle(title);
+      let action: AddChapter;
+      store.scannedActions$.subscribe(a => {
+        if (a.type === ChaptersActionTypes.Add) {
+          action = a as AddChapter;
+        }
+      });
+      fixture.detectChanges();
+
+      // When
+      clickOkButton();
+
+      // Then
+      expect(action).toBeUndefined();
+    });
+
+    it('Chapter should not be added when title is empty.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const chapterNr = '#1';
+      setNr(chapterNr);
+      let action: AddChapter;
+      store.scannedActions$.subscribe(a => {
+        if (a.type === ChaptersActionTypes.Add) {
+          action = a as AddChapter;
+        }
+      });
+      fixture.detectChanges();
+
+      // When
+      clickOkButton();
+
+      // Then
+      expect(action).toBeUndefined();
+    });
+
+    it('Chapter should not be added when nr already exists.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const chapterNr = book2.chapters[0].nr;
+      setNr(chapterNr);
+      const title = 'My title';
+      setTitle(title);
+      let action: AddChapter;
+      store.scannedActions$.subscribe(a => {
+        if (a.type === ChaptersActionTypes.Add) {
+          action = a as AddChapter;
+        }
+      });
+      fixture.detectChanges();
+
+      // When
+      clickOkButton();
+
+      // Then
+      expect(action).toBeUndefined();
+    });
+
+    it('Chapter should not be added when title already exists.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const chapterNr = '#1';
+      setNr(chapterNr);
+      setTitle(book2.chapters[0].title);
+      let action: AddChapter;
+      store.scannedActions$.subscribe(a => {
+        if (a.type === ChaptersActionTypes.Add) {
+          action = a as AddChapter;
+        }
+      });
+      fixture.detectChanges();
+
+      // When
+      clickOkButton();
+
+      // Then
+      expect(action).toBeUndefined();
+    });
+  });
+
+  describe('Error messages', () => {
+    it('By default should not show error messages.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const unexpectedErrorMessages: Array<string> = [
+        i18n.TitleAlreadyInUse + '',
+        i18n.NrAlreadyInuse + '',
+      ];
+
+      // When
+
+      // Then
+      unexpectedErrorMessages.forEach(unexpectedErrorMessage => {
+        expect(nativeElement.innerHTML).not.toContain(unexpectedErrorMessage);
+      });
+      expect(getTitleOfElement(nativeElement, 'input[formcontrolname="nr"]')).toBeFalsy();
+      expect(getTitleOfElement(nativeElement, 'input[formcontrolname="title"]')).toBeFalsy();
+    });
+
+    it('Should display error message that nr is already in use.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const chapterNr = book2.chapters[0].nr;
+      const expectedErrorMessage = i18n.NrAlreadyInuse + '';
+
+      // When
+      setNr(chapterNr);
+      fixture.detectChanges();
+
+      // Then
+      expect(nativeElement.innerHTML).toContain(expectedErrorMessage);
+    });
+
+    it('Should not display error message that nr is already in use.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const chapterNr = '#1';
+      const expectedErrorMessage = i18n.NrAlreadyInuse + '';
+
+      // When
+      setNr(chapterNr);
+      fixture.detectChanges();
+
+      // Then
+      expect(nativeElement.innerHTML).not.toContain(expectedErrorMessage);
+    });
+
+    it('Should display error message that title is already in use.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const title = book2.chapters[0].title;
+      const expectedErrorMessage = i18n.TitleAlreadyInUse + '';
+
+      // When
+      setTitle(title);
+      fixture.detectChanges();
+
+      // Then
+      expect(nativeElement.innerHTML).toContain(expectedErrorMessage);
+    });
+
+    it('Should not display error message that title is already in use.', () => {
+      // Given
+      setSelectedBook(book2.id);
+      const title = 'My title';
+      const expectedErrorMessage = i18n.TitleAlreadyInUse + '';
+
+      // When
+      setTitle(title);
+      fixture.detectChanges();
+
+      // Then
+      expect(nativeElement.innerHTML).not.toContain(expectedErrorMessage);
+    });
   });
 });
--- question-trainer-ng/test/form-controls.ts
+++ question-trainer-ng/test/form-controls.ts
@@ -1,5 +1,3 @@
-import { element } from 'protractor';
-
 export const setSelectValue = (select: HTMLSelectElement, value: string): void => {
   let valueToSet: string;
   const nrOfOptions = select.options.length;
@@ -19,4 +17,12 @@ export const setTextInput = (nativeElement: HTMLElement, query: string, value: s
   const inputControl = nativeElement.querySelector(query) as HTMLInputElement;
   inputControl.value = value;
   inputControl.dispatchEvent(new Event('input'));
-}
+};
+
+export const getTitleOfElement = (nativeElement: HTMLElement, query: string): string => {
+  const element = nativeElement.querySelector(query) as HTMLElement;
+  if (!element) {
+    throw new Error('No element was found by the the following query:\r\n' + query);
+  }
+  return element.title;
+};
